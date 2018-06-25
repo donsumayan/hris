c417cadab27159f239f8b8032235cf3a38c4ab41
donsumayan <don.sumayan@gmail.com>
2018-06-25 18:08:16 +0800

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
     production: true,
     firebase : {
-        apiKey: 'AIzaSyA_lBHDSQpIHFe7GPW5kLpbDJrFFX55hMA',
-        authDomain: 'anything-d42ca.firebaseapp.com',
-        databaseURL: 'https://anything-d42ca.firebaseio.com',
-        projectId: 'anything-d42ca',
-        storageBucket: 'anything-d42ca.appspot.com',
-        messagingSenderId: '151140457822'
     }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,6 @@
 export const environment = {
     production: false,
     firebase : {
-        apiKey: 'AIzaSyA_lBHDSQpIHFe7GPW5kLpbDJrFFX55hMA',
-        authDomain: 'anything-d42ca.firebaseapp.com',
-        databaseURL: 'https://anything-d42ca.firebaseio.com',
-        projectId: 'anything-d42ca',
-        storageBucket: 'anything-d42ca.appspot.com',
-        messagingSenderId: '151140457822'
     }
 };
 
